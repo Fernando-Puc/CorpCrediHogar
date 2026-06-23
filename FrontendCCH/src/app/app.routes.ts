118e59099c39fa081051e8819af7f4f0390f2a9d
Fernando Puc <fernandopuc81@gmail.com>
2026-06-23 13:09:35 -0600

    Creación de componentes. -Se creó el componente sidebar. -Se creó el componente dashboard para el administrador. -Se agregó un diseño previo para el sidebar. -Se instalaron las librerias de angular material y bootstrapp

--- FrontendCCH/src/app/app.routes.ts
+++ FrontendCCH/src/app/app.routes.ts
@@ -11,6 +11,16 @@ export const routes: Routes = [
     path: 'login',
     title: 'login',
     loadComponent: () => import('./ui/login/login').then((c) => c.Login),
-
+  },
+  {
+    title: 'Administrador',
+    path: 'administrador',
+    loadComponent: () => import('./ui/administrador/navbar-admin/navbar-admin').then((c) => c.NavbarAdmin),
+    children: [
+      {
+        path:'',
+        loadComponent: () => import('./ui/administrador/dashboard-adm/dashboard-adm').then((c)=> c.DashboardAdm),
+      }
+    ]
   }
 ];
