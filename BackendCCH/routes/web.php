7be54621b1b6addf06ef9d67bb69c34cd73fca04
Fernando Puc <fernandopuc81@gmail.com>
2026-06-30 16:55:48 -0600

    Backend Empresas -Se creó la migración de empresas. -Se creó el modelo para las empresas. -Se creó el controlador de las empresas. -Se añadieron las funciones Crear, Obtener, Actualizar y Eliminar empresas en empresascontroller.php. -Se agregaron las rutas en el archivo api.php. -Se agregó el archivo api.php. -Se hicieron ajusted en el proyecto.

--- BackendCCH/routes/web.php
+++ BackendCCH/routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmpresasController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Empresas
-
-Route::get('catalogs/empresas/', [EmpresasController::class, 'Obtenerempresas']);
