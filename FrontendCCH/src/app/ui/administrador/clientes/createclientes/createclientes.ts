7a47fcc276a92aead6015b0e10a9147a9b12c4b3
Fernando Puc <fernandopuc81@gmail.com>
2026-07-09 16:59:04 -0600

    Corrección de select -Se instaló la version mas reciente de la libreria nf-select. -Se agregó las versiones en los archivos package.json y package-lock.json -Se agregó el archivo scss global _elements_scss. -En el archivo de estilos globales styles.scss se agregó la importación dell nuevo archivo elements y la libreria de ng-select. -En el componente de createclientes se agreó el ng select estatico como prueba, ajustando el diseño y validadores. -Se cambiaron colores del diseño global del ng select. -Se creó un nuevo modelo llamado catalogs para el interface de tipoContacto.

--- FrontendCCH/src/app/ui/administrador/clientes/createclientes/createclientes.ts
+++ FrontendCCH/src/app/ui/administrador/clientes/createclientes/createclientes.ts
@@ -1,21 +1,31 @@
+import { TipoContacto } from './../../../../core/models/catalogs';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl, FormsModule } from '@angular/forms';
 import { TextInputComponent } from '../../../generic components/input/input.component.';
 import { ActionButtonComponent } from '../../../generic components/actionButton/actionButton.component';
 import { makeRequired } from '../../../../core/validators/makeRequired.validator';
 import { Router } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-createclientes',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, TextInputComponent, ActionButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, TextInputComponent, ActionButtonComponent, NgSelectModule, FormsModule, CommonModule],
   templateUrl: './createclientes.html',
   styleUrl: './createclientes.scss',
 })
 export class Createclientes {
 
   clientes: any[] = [];
+  TipoContacto: TipoContacto[] = [
+    {
+    id: 1,
+    descripcion: 'cliente',
+    }
+  ];
   formGroup: FormGroup;
 
   constructor(private fb: FormBuilder, private router: Router){
@@ -27,6 +37,7 @@ export class Createclientes {
       direccion: new FormControl('', [makeRequired]),
     });
   }
+
     guardar() {
     if (this.formGroup.invalid) {
       this.formGroup.markAllAsTouched();
