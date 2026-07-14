59338be63cd0d3b3d6a8994a0b9c6408ddf2a61d
Fernando Puc <fernandopuc81@gmail.com>
2026-07-14 11:25:55 -0600

    Backend Catálogo Marcas -Se creó la migración para la tabla de marca_productos. -Se creó el modelo para las Marcas. -Se creó el controlador MarcaProductosController agregando las 4 funciones obtener, agregar, actualizar y eliminar. -Se agregaron las rutas en el archivo api.php ya funcionales.

--- BackendCCH/routes/api.php
+++ BackendCCH/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpresasController;
 use App\Http\Controllers\LineaProductosController;
+use App\Http\Controllers\MarcaProductosController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +21,9 @@ Route::get('catalogs/lineas/', [LineaProductosController::class, 'ObtenerLineas'
 Route::post('catalogs/crearlinea/', [LineaProductosController::class, 'CrearLinea']);
 Route::put('catalogs/actualizarlinea/{id}', [LineaProductosController::class, 'ActualizarLinea']);
 Route::delete('catalogs/eliminarlinea/{id}', [LineaProductosController::class, 'EliminarLinea']);
+
+//Marcas de Productos
+Route::get('catalogs/marcas/', [MarcaProductosController::class, 'ObtenerMarcas']);
+Route::post('catalogs/crearmarca/', [MarcaProductosController::class, 'CrearMarca']);
+Route::put('catalogs/actualizarmarca/{id}', [MarcaProductosController::class, 'ActualizarMarca']);
+Route::delete('catalogs/eliminarmarca/{id}', [MarcaProductosController::class, 'EliminarMarca']);
